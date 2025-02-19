857e0d613a8299dadfe8cd661e135a4d12236434
Gabriel <ginsberg.gabriel@gmail.com>
2025-02-19 15:05:02 -0500

    merged files over to new repo

---
+++ contracts/src/IBusinessRewards.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title Business Rewards Interface
+/// @dev This interface defines the functions a business needs to reward customers
+interface IBusinessRewards {
+    
+}
