539bb989cdac7ccdf29bca3f47f0d29522092891
Jida Li <77774296+jidalii@users.noreply.github.com>
2025-02-20 12:44:07 -0500

    updated

--- contracts/src/IDProtocol.sol
+++ contracts/src/IDProtocol.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC20.sol";
 
 import "./verifier.sol";
 import "./MerchantContract.sol";
