4054b31fc0c89749f320cf319b7637b152a4205d
Gabriel <ginsberg.gabriel@gmail.com>
2025-02-20 16:45:29 -0500

    feat: commented the mock oracle

--- contracts/src/MockOracle.sol
+++ contracts/src/MockOracle.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.13;
 
+/// @title A contract representing a pricing oracle for ERC20
 contract MockOracle {
-    mapping(address => uint) oracle; // token address -> value:1ETH ratio
+    /// @notice the oracle mapping (token address => value per ETH)
+    mapping(address => uint) oracle;
+    
+    /**
+     * @dev the constructor that hard codes oracle vals for tokens
+     * This is purely for demonstration purposes
+     */
     constructor(){
         oracle[address(1)] = 5;
-        oracle[address(2)] = 10;
+        oracle[address(2)] = 1/2;
         oracle[address(3)] = 2;
     }
 
+    /**
+     * @notice the function merchants will call to determine the value in ETH of the token
+     * @param address tokenAddr: address of the token being queried
+     * @return uint the value in ETH of a single unit of the token
+     */
     function calcRatio(address tokenAddr) external returns(uint){
         return oracle[tokenAddr];
     }
