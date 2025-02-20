5df5408680185543efdc4da3957b769401eadd96
Gabriel <ginsberg.gabriel@gmail.com>
2025-02-20 13:11:57 -0500

    updated MerchantContract

--- contracts/src/MerchantContract.sol
+++ contracts/src/MerchantContract.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "./IDProtocol.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./MockOracle.sol";
 
 contract MerchantContract {
     /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~merchant data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
